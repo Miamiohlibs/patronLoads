7e6686ad32b071a51a5229ccec0435a28133a757
craigboman <craig.boman@gmail.com>
2018-10-30 16:52:36 -0400

    mostly done looping; need error checking'

--- parse.py
+++ parse.py
@@ -1,11 +1,13 @@
 import os, csv
 import numpy as np
-from jsonschema import validate #use to validate each array slice writing to api
-import get_token
+from jsonschema import validate  #use to validate each array slice writing to api
+import urllib.parse import urlparse  #url encoding
+from get_token import get_token
 
 ##the purpose of this file is to parse a basic tsv patron file given to a
 ##library from central IT containing all of the patrons into json api calls
 ##to the Sierra patron api
+
 
 def parse():
     datafile = open('patrons.csv', 'r')
@@ -17,15 +19,9 @@ def parse():
         data.append(row)
     datafile.close()
 
-    b = np.reshape(data, (-1,11)) #reshapes as nested array on 11 columns each
+    b = np.reshape(data, (-1,11))  #reshapes as nested array on 11 columns each
     #remove trailing white space; no need to reshape or trim since we can do this during schema write
     b = np.char.rstrip(b, ' ')
-
-    #store raw ptfs data in array; shouldn't need to store post-split array
-    #use array slices during writing to schema
-    #for i in b:
-    #    slice = i[0][15:]
-    #    np.insert(i,0,slice)
 
     #outputs new np array but inserting the expirationDate as an additional array slice
     patron = {}
@@ -53,10 +49,10 @@ def parse():
 
         #building patron json file for each 11 row patron record from patron.csv
         patron["expirationDate"] = i[0][16:]
-        patron["patronType"] = i[0][3] #0002
-        patron["patronCodes"] = {} #will be filled below
-        patron["patronCodes"]["pcode1"] = i[0][4] #00028
-        patron["patronCodes"]["pcode2"] = i[0][5] #000031
+        patron["patronType"] = i[0][3]  #0002
+        patron["patronCodes"] = {}  #will be filled below
+        patron["patronCodes"]["pcode1"] = i[0][4]  #00028
+        patron["patronCodes"]["pcode2"] = i[0][5]  #000031
         patron["patronCodes"]["pcode3"] = i[0][6]
         #fieldTags
         plus = {"fieldTag":"b","content":i[9][1:]}
@@ -67,24 +63,40 @@ def parse():
         address = {"fieldTag":"a","content":i[2][1:]}
         home = {"fieldTag":"h","content":i[4][1:]}
         name = {"fieldTag":"n","content":i[1][1:]}
-        campusPhone = {"fieldTag":"t","content":i[3][1:]}
+        campus = {"fieldTag":"t","content":i[3][1:]}
         phone = {"fieldTag":"p","content":i[5][1:]}
-        patron["varFields"] = [plus,barcode,soc,uniqueId,email,address,home,name,campusPhone,phone]
+        patron["varFields"] = [plus,barcode,soc,uniqueId,email,address,home,name,campus,phone]
         #print(patron)
         print(patron)
         #validate json schema; look for raised exception
         #if
         validate(patron,schema)
 
+        #begin api call
+        headers = {
+            'authorization': str(get_token()),
+            'cache-control': "no-cache",
+            'postman-token': "715478e1-10c5-8bc7-7758-415c1be73131",
+            'content-type': "application/json"
+        }
+
 
             #lookup to see if this is a new patron; %2b is the + symbol converted, add the soc
-            url = "https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/patrons/find?varFieldTag=s&varFieldContent={}".format(i[6][1:])
+        url = "https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/patrons/find?varFieldTag=s&varFieldContent=%2b{}".format(i[6][2:]))
             #need to url encode url
+            #response = requests.get(url, headers=headers, params=querystring)
+        response = requests.get(url, headers = headers)
+        if response.status_code == requests.codes.ok:  #write patron ID response
+            id = response.json()["id"]
+            url = "https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/patrons/{}".format(id)
 
-            #if patron is found retrieve Sierra patron ID number
-            #preprend Sierra patron ID to the beginning of the patron json file
             #then write to Sierra update patron API
-            url = "https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/patrons/{}".format(SierraPatronID)
-
-            #if patron is not located write to create application
+            response = requests.put(url, headers=headers, json=patron)
+            #if response.raise_for_status() == ''
+        elif response.status_code == requests.codes.not_found:  #post new patron
             url = "https://holmes.lib.miamioh.edu:443/iii/sierra-api/v4/patrons/"
+            response = requests.post(url, headers=headers, json=patron)
+
+        ##do i need another elif for error logging
+
+#parse()
